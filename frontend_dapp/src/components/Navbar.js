ed369fe0d4fd428dd1760ede52bd915b68e8f0ea
Faytey7 <Faithroberts708@gmail.com>
2023-04-11 02:16:04 +0100

    adding babel support

--- frontend_dapp/src/components/Navbar.js
+++ frontend_dapp/src/components/Navbar.js
@@ -1,8 +1,8 @@
-import { Inter } from 'next/font/google'
-import { ConnectButton } from '@rainbow-me/rainbowkit'
-import Link from 'next/link'
+import { Inter } from "next/font/google";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Navbar() {
   return (
@@ -11,15 +11,15 @@ export default function Navbar() {
         <Link href={"/"}>BUNZZ</Link>
       </div>
       <div>
-        <ul className='space-x-7 text-lg'>
+        <ul className="space-x-7 text-lg">
           <Link href={"/"}>Home</Link>
           <Link href={"/about"}>About</Link>
-          <Link href={"/contactus"}>Contact Us</Link>
+          <Link href={"/launchpad"}>Launch Pad</Link>
           <Link href={"/marketplace"}>Market Place</Link>
           <Link href={"/profile"}>Profile</Link>
         </ul>
       </div>
-      <ConnectButton/>
+      <ConnectButton />
     </main>
-  )
+  );
 }
---
+++ frontend_dapp/src/pages/launchpad.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Launchpad() {
+  return (
+    <div className="">
+      <p>This is the launchpad page</p>
+    </div>
+  );
+}
