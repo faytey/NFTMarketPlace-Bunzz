a6aab21f622cf9d3849dbdec630fb2119d3139e3
Pato-web-development <patrickmaebi@gmail.com>
2023-04-12 17:12:49 +0100

    update

--- frontend_dapp/src/pages/index.js
+++ frontend_dapp/src/pages/index.js
@@ -5,10 +5,10 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="mt-6 " style = {{backgroundImage:"linear-gradient(20deg, blue, green, red)", transition:"3s ease-in-out"}}>
-      <div className="grid grid-cols-2 pb-6" id="firstHomeSection" style={{transition:"3s ease-in-out"}}>
+    <main className="mt-6" style = {{transition:"3s ease-in-out"}}>
+      <div className="grid grid-cols-2 pb-6" id="firstHomeSection" style={{transition:"3s ease-in-out", paddingLeft:"12%", paddingRight:"12%"}}>
       
-      <div className = "grid-cols-1" style={{backgroundColor:"", paddingLeft:"23%",
+      <div className = "grid-cols-1" style={{backgroundColor:"",
       paddingRight:"10%", paddingTop:"5%", paddingBottom:"5%",}}>
       <h1 style={{fontSize:"350%", fontWeight:"bold"}}>Discover Digital Art & Collect Nfts</h1>
       <p style={{fontSize:"120%",}}>Nft Marketplace Ui Created With Anima For Figma. Collect, Buy And Sell Art From More Than 20k Nft Artists.</p>
@@ -33,14 +33,18 @@ export default function Home() {
       </div>
 
       <div className = "grid-cols-1 " style={{backgroundColor:"", paddingLeft:"10%", 
-      paddingRight:"23%", paddingTop:"5%", paddingBottom:"5%"}}>
+       paddingTop:"5%", paddingBottom:"5%"}}>
       <img src="././assets/benz.jpg" className = "" id="homepagefirstPic" style={{transition:"5s ease-in-out",
        boxShadow:"15px 15px 10px 2px rgba(20,20,20,0.9)", animation:"homepicanimation2 3s alternate infinite",borderRadius:"" }} />
       </div>
       
       </div>
 
+      <div className = "mt-4" id="secondHomeSection" style={{paddingLeft:"12%", paddingRight:"12%"}}>
+        <h1>Trending Collection</h1>
+        <p>Checkout Our Weekly Updated Trending Collection.</p>
 
+      </div>
 
     </main>
   )
