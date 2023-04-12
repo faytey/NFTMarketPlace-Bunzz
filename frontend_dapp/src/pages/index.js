cd6a1c8d58d3dfbac8a88c6141d8a1293d3e340d
Pato-web-development <patrickmaebi@gmail.com>
2023-04-12 03:57:13 +0100

    first section of homepage done

--- frontend_dapp/src/pages/index.js
+++ frontend_dapp/src/pages/index.js
@@ -6,22 +6,43 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className="mt-6 " style = {{backgroundImage:"linear-gradient(20deg, blue, green, red)"}}>
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 pb-6">
       
       <div className = "grid-cols-1" style={{backgroundColor:"", paddingLeft:"23%", 
       paddingRight:"10%", paddingTop:"5%", paddingBottom:"5%",}}>
       <h1 style={{fontSize:"350%", fontWeight:"bold"}}>Discover Digital Art & Collect Nfts</h1>
       <p style={{fontSize:"120%",}}>Nft Marketplace Ui Created With Anima For Figma. Collect, Buy And Sell Art From More Than 20k Nft Artists.</p>
+      <button class="mt-8 font-bold py-4 px-8 rounded-full"
+      style={{transition:"1s ease-in-out",background:"#a244ff", hover:"transform:scale3d(0.8,0.8,0.8)",}}>
+      Get Started
+      </button>
+      <p className="grid grid-cols-3 mt-8" style={{fontSize:"120%",}} >
+      <p className="grid-cols-1">
+      <span style={{fontSize:"120%", fontWeight:"bold"}}>240k+</span><br></br>
+      Total Sale
+      </p>
+      <p className="grid-cols-1">
+      <span style={{fontSize:"120%", fontWeight:"bold"}}>100k+</span><br></br>
+      Auctions
+      </p>
+      <p className="grid-cols-1">
+      <span style={{fontSize:"120%", fontWeight:"bold"}}>240k+</span><br></br>
+      Artists
+      </p>
+      </p>
       </div>
-      <div className = "grid-cols-1" style={{backgroundColor:"", paddingLeft:"10%", 
+
+      <div className = "grid-cols-1 " style={{backgroundColor:"", paddingLeft:"10%", 
       paddingRight:"23%", paddingTop:"5%", paddingBottom:"5%"}}>
-      <img src="././assets/benz.jpg" style={{transition:"5s ease-in-out",
+      <img src="././assets/benz.jpg" className = "" style={{transition:"5s ease-in-out",
        boxShadow:"15px 15px 10px 2px rgba(20,20,20,0.9)", animation:"homepicanimation 5s alternate infinite",
-       animation:"homepicanimation2 3s alternate infinite", }} />
-    
+       animation:"homepicanimation2 3s alternate infinite",borderRadius:"" }} />
       </div>
       
       </div>
+
+
+
     </main>
   )
 }
